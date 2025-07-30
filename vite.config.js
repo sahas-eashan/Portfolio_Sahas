96910353828cd2825850704364f8acb7e87c6277
sahas-eashan <eashansgs.22@uom.lk>
2025-07-30 22:38:10 +0530

    all

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  //base: '/Fortfolio_Sahas/', // 👈 MUST MATCH the repo name exactly!
+  base: '/Fortfolio_Sahas/', // 👈 MUST MATCH the repo name exactly!
 });
